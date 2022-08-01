fcdfbdcb6a6c1309052bfd37642dee67ab566dbe
Lymond Glenda <lymondgl@ge-f6.kzn.21-school.ru>
2022-08-01 22:52:32 +0300

    part_4

---
+++ coolt.py
@@ -0,0 +1,45 @@
+def check(text):
+    global memory
+    print(text)
+    calc = input()
+    x, oper, y = calc.split()
+    if x == "M":
+        x = memory
+    if y == "M":
+        y = memory
+    try:
+        float(x), float(y)
+        if oper not in ["+", "-", "*", "/"]:
+            print("Yes ... an interesting math operation. You've slept through all classes, haven't you?")
+            check(text)
+        else:
+            if oper == "+":
+                store_and_continue((float(x) + float(y)))
+            if oper == "-":
+                store_and_continue((float(x) - float(y)))
+            if oper == "*":
+                store_and_continue((float(x) * float(y)))
+            if oper == "/":
+                if float(y) == 0:
+                    print("Yeah... division by zero. Smart move...")
+                    check(text)
+                else:
+                    store_and_continue((float(x) / float(y)))
+    except ValueError:
+        print('Do you even know what numbers are? Stay focused!')
+        check(text)
+
+
+def store_and_continue(result):
+    global memory
+    print(result)
+    print("Do you want to store the result? (y / n):")
+    if input() == "y":
+        memory = result
+    print("Do you want to continue calculations? (y / n):")
+    if input() == "y":
+        check("Enter an equation")
+
+
+memory = 0
+check("Enter an equation")
---
+++ src/04/calc.py
@@ -0,0 +1,132 @@
+
+msg_0 = "Enter an equation"
+msg_1 = "Do you even know what numbers are? Stay focused!"
+msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
+msg_3 = "Yeah... division by zero. Smart move..."
+msg_4 = "Do you want to store the result? (y / n):"
+msg_5 = "Do you want to continue calculations? (y / n):"
+msg_6 = " ... lazy"
+msg_7 = " ... very lazy"
+msg_8 = " ... very, very lazy"
+msg_9 = "You are"
+
+memory = 0.0
+lexems = ['+', '-', '/', '*']
+
+
+def is_one_digit(var_1) -> bool:
+    if var_1.is_integer():
+        return True
+    return False
+
+
+def ask_user(var) -> None:
+    global memory
+    print(msg_4)
+    user_answer = input()
+    print(msg_5)
+    if user_answer == "y":
+        memory = var
+        user_answer_2 = input()
+        if user_answer_2 == "y":
+            with_memmory()
+    elif user_answer == 'n':
+        user_answer_2 = input()
+        if user_answer_2 == 'y':
+            enter()
+
+
+def with_memmory() -> None:
+    result = 0.0
+    print(msg_0)
+    user_input = input().split()
+    try:
+        x = float(user_input[0])
+        oper = str(user_input[1])
+        if oper not in lexems:
+            print(msg_2)
+            with_memmory()
+        if oper == "+":
+            result = x + memory
+            print(result)
+            ask_user(result)
+        elif oper == "-":
+            result = x - memory
+            print(result)
+            ask_user(result)
+        elif oper == "*":
+            result = x * memory
+            print(result)
+            ask_user(result)
+        elif oper == "/":
+            result = x / memory
+            print(result)
+            ask_user(result)
+    except ValueError:
+        print(msg_1)
+        with_memmory()
+    except SyntaxError:
+        print(msg_2)
+        with_memmory()
+    except ZeroDivisionError:
+        print(msg_3)
+        with_memmory()
+    except IndexError:
+        print("Man")
+        with_memmory()
+
+
+def enter():
+    result = 0.0
+    print(msg_0)
+    user_input = input().split()
+    y = user_input[2]
+    if y == "M":
+        y = memory
+    else:
+        try:
+            y = float(user_input[2])
+        except ValueError:
+            print(msg_1)
+            enter()
+        except SyntaxError:
+            print(msg_2)
+            enter()
+    try:
+        x = float(user_input[0])
+        oper = str(user_input[1])
+        if oper not in lexems:
+            print(msg_2)
+            enter()
+        if oper == "+":
+            result = x + y
+            print(result)
+            ask_user(result)
+        elif oper == "-":
+            result = x - y
+            print(result)
+            ask_user(result)
+        elif oper == "*":
+            result = x * y
+            print(result)
+            ask_user(result)
+        elif oper == "/":
+            result = x / y
+            print(result)
+            ask_user(result)
+    except ValueError:
+        print(msg_1)
+        enter()
+    except SyntaxError:
+        print(msg_2)
+        enter()
+    except ZeroDivisionError:
+        print(msg_3)
+        enter()
+    except IndexError:
+        print("Man")
+        enter()
+
+
+if __name__ == "__main__":
+    enter()
